624830bc8abb7caf3c845b96cdebc42f0b42a6cc
ted Liefeld <liefeld@broadinstitute.org>
2017-08-31 14:34:17 -0700

    First version working on docker and batch. Validates to creaet a transcriptome index

--- runS3Batch_prerun_custom.sh
+++ runS3Batch_prerun_custom.sh
@@ -1,13 +1,18 @@
 
-
 ##################################################
-# MODIFICATION FOR  LOADING GENOME FILE FOR TOPHAT FROM S3
+# MODIFICATION FOR  LOADING GENOME INDEX Directory FOR TOPHAT FROM S3
 ##################################################
 
 indexDir=$(python /usr/local/bin/findIndex.py $@)
-echo "Index dir is in shell " $indexDir
+mkdir -p $indexDir
 
 # mount pre-compiled libs from S3
-echo "FOR TopHat. 13 CUSTOMIZING: aws s3 sync $S3_ROOT$indexDir $indexDir --quiet"
+echo "13. FOR TopHat customizations: Sync the Genome Index to the container,   aws s3 sync $S3_ROOT$indexDir $indexDir --quiet"
+aws s3 sync $S3_ROOT$indexDir $indexDir --quiet
+
+echo 14. ls on index follows,  ls /xchip/gpdev/gpftp/pub/module_support_files/bowtie2/index/by_genome/Homo_sapiens_hg19_UCSC/ 
+ls -lrt /xchip/gpdev/gpftp/pub/module_support_files/bowtie2/index/by_genome/Homo_sapiens_hg19_UCSC/
+
+
 
 
--- tests/tophat/runConfig.sh
+++ tests/tophat/runConfig.sh
@@ -4,12 +4,10 @@ TOPHATDIR=/bin/tophat-2.0.14.Linux_x86_64/
 INDEXDIR=/xchip/gpdev/gpftp/pub/module_support_files/bowtie2/index/by_genome/Homo_sapiens_hg19_UCSC/
 BOWTIEDIR=/bin/bowtie2-2.2.3/
 SAMTOOLSDIR=/usr/bin/
-ANNOT_DIR=/Users/liefeld/GenePattern/gp_dev/docker/docker-tophat/tests/tophat/indices/annotation
-INPUT_FILE_DIRECTORIES=/Users/liefeld/GenePattern/gp_dev/docker/docker-tophat/tests/tophat/indices/annotation
+INPUT_FILE_DIRECTORIES=$PWD/data
 
 
-TASKLIB=$PWD/src
-INPUT_FILE_DIRECTORIES=$PWD/indices/annotation
+TASKLIB=$PWD/src/
 
 COMMAND_LINE="perl $TASKLIB/tophat_wrapper.pl --tophatDir $TOPHATDIR --samtoolsDir $SAMTOOLSDIR --bowtieDir $BOWTIEDIR --libdir $TASKLIB --index $INDEXDIR --pair1 --pair2 --mateDist 50 --mateStd --readEditDist --readGapLength --oprefix 1470911_null --gtf $INPUT_FILE_DIRECTORIES/Homo_sapiens_hg19_UCSC.gtf --transcriptomeIndex --juncs --transcriptomeOnly no --minALen 8 --maxSpliceM 0 --minILen --maxILen 500000 --maxInsLen 3 --maxDelLen 3 --maxMHit 20 --readMis 2 --maxTHit --fAnchorLen 20 --fReadMis 2 --fusion -- --no-coverage-search -p 2"
 
